1e4f154350ae142d91c020d4c02cf7e5cc72c846
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2021-11-30 13:51:13 +0100

    comment

--- AdvancedIT_Java/src/de/mathisneunzig/advancedIT/Main.java
+++ AdvancedIT_Java/src/de/mathisneunzig/advancedIT/Main.java
@@ -4,14 +4,16 @@ import de.mathisneunzig.advancedIT.aufgaben.*;
 
 public class Main {
 	
+	// Die auszuführende Aufgabe auskommentieren!
 	public static void main(String[] args) {
 		
 //		Aufgabe8.start();
-		try {
-			Aufgabe9.start();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		
+//		try {
+//			Aufgabe9.start();
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
 		
 	}
 	
